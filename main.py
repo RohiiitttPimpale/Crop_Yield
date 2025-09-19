d210943c7617d8bc0ab2b570c599456b6a8c841d
RohiiitttPimpale <rohitpimpale2095@gmail.com>
2025-09-20 00:20:25 +0530

    change db commit

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class User(db.Model, UserMixin):
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db.init_app(app=app)
 app.secret_key = os.environ.get("FLASK_KEY") # Add secret key for flash messages
 
